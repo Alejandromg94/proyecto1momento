4ba10baac9ebc3897a55d786be6b528809fcf1c2
Alejandromg94 <Alejandromg94@outlook.com>
2026-03-06 18:58:53 -0500

    Agregar funciones para registrar notas de estudiantes y validar entradas

--- Main.py
+++ Main.py
@@ -8,3 +8,22 @@ def registrar_ingreso():
     else:
         estudiantes[nombre] = []
         print("Estudiante registrado correctamente.")
+        
+def registrar_nota():
+    nombre = input("Ingrese el nombre del estudiante: ")
+    
+    if nombre not in estudiantes:
+        print("El estudiante no está registrado.")
+        return
+    
+    try:
+        nota = float(input("Ingrese la nota (0 a 5): "))
+        
+        if 0 <= nota <= 5:
+            estudiantes[nombre].append(nota)
+            print("Nota registrada correctamente.")
+        else:
+            print("La nota debe estar entre 0 y 5.")
+    
+    except ValueError:
+        print(" Debe ingresar un número válido.")
